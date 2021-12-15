ff558624a7aadd66c4190b7f175acc8158ccfbc9
19PA1A0535 <60499415+19PA1A0535@users.noreply.github.com>
2021-12-15 09:20:12 +0530

    Create move1.java

---
+++ move1.java
@@ -0,0 +1,12 @@
+import exception.InvalidPositionException;
+
+public class DefaultMove extends Move {
+
+    public DefaultMove() {
+        super(0);
+    }
+    @Override
+    public boolean isValidPosition(int cellPosition) throws InvalidPositionException {
+        return false;
+    }
+}
